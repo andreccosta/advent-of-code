7c371b4699c8bfc1c314138be3932351968d7ed7
André Costa <andreccosta@me.com>
2019-12-26 00:54:31 +0000

    day1

---
+++ 2019/day1/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const reader = require('readline')
+
+let totalFuel = 0
+const requiredFuel = mass => Math.floor(mass / 3) - 2
+
+function totalFuelForModule(mass) {
+  let fuel = requiredFuel(mass)
+  let additionalFuel = fuel
+
+  while(true) {
+    additionalFuel = requiredFuel(additionalFuel)
+
+    if (additionalFuel <= 0)
+      break;
+
+      fuel += additionalFuel  
+  }
+
+  return fuel
+}
+
+const lineReader = reader.createInterface({
+  input: fs.createReadStream('input.txt'),
+  terminal: false
+})
+
+lineReader.on('line', line => totalFuel += totalFuelForModule(line))
+lineReader.on('close', _ => console.log(`totalFuel: ${totalFuel}`))

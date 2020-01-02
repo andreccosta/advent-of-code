fecb829c131db2f366a741cd0b403763b688a1cc
André Costa <andreccosta@me.com>
2020-01-02 08:31:32 +0000

    update day9 with part2

--- 2019/day9/index.js
+++ 2019/day9/index.js
@@ -6,10 +6,7 @@ const fileContents = buffer.toString().trim()
 
 const IntCode = require('./intCode')
 
-//let intCode = new IntCode('109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99')
-// let intCode = new IntCode('1102,34915192,34915192,7,4,7,99,0')
-// let intCode = new IntCode('104,1125899906842624,99')
 let intCode = new IntCode(fileContents)
-intCode.process([1])
+intCode.process([2])
 
 console.log(intCode.outputs)

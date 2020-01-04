8496e8fe610722a82cde9ea64364eda7ff4b399a
André Costa <andreccosta@me.com>
2020-01-04 00:00:48 +0000

    add day10 part

--- 2019/day10/index.js
+++ 2019/day10/index.js
@@ -14,7 +14,7 @@ asteroidsMap.forEach((row, y) =>
   })
 )
 
-const result = asteroids.map(({ x, y }) => {
+const station = asteroids.map(({ x, y }) => {
   const angles = new Set()
 
   asteroids.forEach(({ x: z, y: w }) => {
@@ -30,4 +30,33 @@ const result = asteroids.map(({ x, y }) => {
   }
 }).sort((x, y) => y.asteroids - x.asteroids)[0]
 
-console.log(result)
+console.log('best station location', station)
+
+let targets = asteroids.map(({ x, y }) => ({
+    x,
+    y,
+    angle: Math.atan2(y - station.y, x - station.x) * (180 / Math.PI),
+    distance: Math.hypot(x - station.x, y - station.y)
+  })
+).sort((a, b) => a.angle - b.angle)
+
+const angles = [...new Set(targets.map(t => t.angle))]
+let angleIndex = angles.findIndex(angle => angle == -90)
+let targetCount = 0
+
+while (targets.length) {
+  const target = targets
+    .filter(t => t.angle == angles[angleIndex])
+    .sort((a, b) => a.distance - b.distance)[0]
+
+  if (target) {
+    targets = targets.filter(({ x, y }) => !(x == target.x && y == target.y))
+    targetCount++
+
+    if (targetCount == 200) {
+      console.log('200th asteroid checksum', target.x * 100 + target.y)
+    }
+  }
+
+  angleIndex = angleIndex < angles.length ? angleIndex + 1 : 0
+}

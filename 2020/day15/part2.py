090db8fb4c21fbe141be5ea04d2aca6ae74978c5
André Costa <andreccosta@me.com>
2020-12-15 13:26:33 +0000

    add 2020 day15 part1 and part2

---
+++ 2020/day15/part2.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+file = open('input.txt', 'r')
+start = [int(n) for n in file.read().strip().split(',')]
+
+memory = dict()
+target = 30000000
+
+
+def _simulate():
+    for i, n in enumerate(start):
+        memory[n] = deque([i, None], 2)
+
+    for i in range(i + 1, target):
+        if memory[n] is None:
+            memory[0].appendleft(i)
+            n = 0
+        elif memory[n].count(None) > 0:
+            n = 0
+            memory[n].appendleft(i)
+        else:
+            n = memory[n][0] - memory[n][1]
+
+            if n in memory:
+                memory[n].appendleft(i)
+            else:
+                memory[n] = deque([i, None], 2)
+
+    return n
+
+
+print(_simulate())

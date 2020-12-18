c84421676894aea48c62ffce0f6c7aa99dc90f42
André Costa <andreccosta@me.com>
2020-12-18 12:18:09 +0000

    add 2020 day18 part1 and part2

---
+++ 2020/day18/part1.py
@@ -0,0 +1,65 @@
+import collections
+import re
+
+file = open('input.txt', 'r')
+lines = file.read().splitlines()
+
+
+def _findclosing(exp, i):
+    d = collections.deque()
+
+    if exp[i] != '(':
+        raise ValueError('unexpected start char')
+
+    for k in range(i, len(exp)):
+        if exp[k] == ')':
+            d.popleft()
+
+        elif exp[k] == '(':
+            d.append(exp[i])
+
+        if not d:
+            return k
+
+    return -1
+
+
+def _calculate(exp):
+    pos = 0
+    total = 0
+    op = '+'
+
+    while pos < len(exp):
+        if exp[pos].isspace():
+            pass
+
+        if exp[pos] == '(':
+            start = pos
+            end = _findclosing(exp, pos)
+
+            if op == '+':
+                total += _calculate(exp[start + 1: end])
+            else:
+                total *= _calculate(exp[start + 1: end])
+
+            pos = end
+
+        if exp[pos] in '+*':
+            op = exp[pos]
+
+        if exp[pos].isdigit():
+            if op == '+':
+                total += int(exp[pos])
+            else:
+                total *= int(exp[pos])
+
+        pos += 1
+
+    return total
+
+
+total = 0
+for expression in lines:
+    total += _calculate(expression)
+
+print(total)

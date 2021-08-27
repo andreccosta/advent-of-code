633151d5de2eac620b08c1907e4f5ea1011193e9
André Costa <andreccosta@me.com>
2021-08-28 00:53:15 +0100

    add 2015 day 9

---
+++ 2015/day9/part1.py
@@ -0,0 +1,28 @@
+import collections
+import itertools
+import re
+
+content = open('input.txt', 'r').read()
+lines = content.strip().splitlines()
+
+map = collections.defaultdict(
+    lambda: collections.defaultdict(
+        lambda: float("inf")
+    )
+)
+
+for line in lines:
+    matches = re.match(r'(\w+) to (\w+) = (\d+)', line)
+    origin, destination, distance = matches.groups()
+    distance = int(distance)
+
+    map[origin][destination] = distance
+    map[destination][origin] = distance
+
+length = min(
+    sum(map[origin][destination]
+        for origin, destination in zip(path, path[1:]))
+    for path in itertools.permutations(map.keys())
+)
+
+print(length)

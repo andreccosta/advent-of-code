f7b16d97f3f8e1811ac6b0f5bd1fda7b03ae7000
André Costa <andreccosta@me.com>
2022-12-11 11:00:59 +0000

    add 2022 day 11

---
+++ 2022/day11/part1.rb
@@ -0,0 +1,63 @@
+input = File
+  .read("input.txt")
+  .split("\n\n")
+  .map { |attrs|
+    attrs.split("\n").map { |l|
+      l.split(": ").last.strip
+    }
+  }
+
+ROUNDS = 20
+
+class Monkey
+  attr_reader :modulo, :inspect_count
+
+  def initialize(items_str, op_str, mod_str, if_true_str, if_false_str)
+    @items = items_str.split.map(&:to_i)
+    @op =
+      case op_str.split(" = ")[1].split
+      in ["old", "*", "old"]
+        ->(x) { x * x }
+      in ["old", "*", v]
+        ->(x) { x * v.to_i }
+      in ["old", "+", v]
+        ->(x) { x + v.to_i }
+      end
+
+    @modulo = mod_str.split.last.to_i
+    @if_true = if_true_str.split.last.to_i
+    @if_false = if_false_str.split.last.to_i
+
+    @inspect_count = 0
+  end
+
+  def inspect(&block)
+    until @items.empty?
+      worry = @op[@items.shift] / 3
+
+      if worry % @modulo == 0
+        block.call(worry, @if_true)
+      else
+        block.call(worry, @if_false)
+      end
+
+      @inspect_count += 1
+    end
+  end
+
+  def receive(item)
+    @items.push(item)
+  end
+end
+
+monkeys = input.map { Monkey.new(*_1[1..]) }
+
+ROUNDS.times do
+  monkeys.each do |monkey|
+    monkey.inspect do |worry, target|
+      monkeys[target].receive(worry)
+    end
+  end
+end
+
+p monkeys.map(&:inspect_count).max(2).inject(:*)

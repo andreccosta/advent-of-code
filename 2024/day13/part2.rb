5afb646f8286974c913e014c71a224478ad5ce82
André Costa <andreccosta@me.com>
2024-12-13 09:15:03 +0000

    add 2024 day 13

---
+++ 2024/day13/part2.rb
@@ -0,0 +1,47 @@
+require 'matrix'
+
+data = File.read("input.txt")
+machines = data.split("\n\n").map { _1.split("\n")}
+
+def parse_button(button)
+  Vector[
+    button[/X\+(\d+)/, 1]&.to_i,
+    button[/Y\+(\d+)/, 1]&.to_i
+  ]
+end
+
+def parse_prize(prize)
+  Vector[
+    10000000000000 + prize[/X\=(\d+)/, 1]&.to_i,
+    10000000000000 + prize[/Y\=(\d+)/, 1]&.to_i
+  ]
+end
+
+def parse(machine)
+  button_a, button_b, prize = machine
+
+  [
+    parse_button(button_a),
+    parse_button(button_b),
+    parse_prize(prize)
+  ]
+end
+
+tokens = machines.sum do |machine|
+  button_a, button_b, prize = parse(machine)
+
+  m = Matrix.columns([button_a, button_b])
+
+  if m.regular?
+    w = m.inverse * prize
+    wa, wb = w[0], w[1]
+
+    if wa.denominator == 1 && wb.denominator == 1
+      next 3 * wa.to_i + 1 * wb.to_i
+    end
+  end
+
+  0
+end
+
+p tokens
